dbc4d3cb83e4db983e5f17720c01c3d798a2b0e9
Lynn Edward McQueen <lynn.edward.mcqueen@gmail.com>
2023-05-04 19:26:06 -0600

    Reads button information from database

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 console.log("Here in index.js")
+
+document.addEventListener("DOMContentLoaded", () => {
+    let flowers = ["peony", "rose", "sunflower", "linaria"]
+
+    flowers.forEach(callbackFunc);
+
+    function callbackFunc(name, idx, arr){
+        console.log(name, idx, arr)
+    }
+
+    fetch( "http://localhost:3000/buttons")
+    .then(result => result.json())
+    .then(buttons => buttons.forEach(configureButton))
+    //.then((buttons) => console.log(buttons))
+
+})
+
+
+function configureButton(button) {
+    console.log(button);
+} 
+
+function configureButtons(buttons) {
+//    console.log(buttons)
+    buttons.forEach(configureButton)
+    
+}
